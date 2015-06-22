fbfb8105957b2d64d5869913b5afd0da2cfc6d34
Bob Pace <bob.pace@gmail.com>
2015-06-22 23:15:56 +0000

    need to do inverse mod 2, using naive method right now which only works for 3x3 and smaller

--- test/mocha/example-tests.js
+++ test/mocha/example-tests.js
@@ -24,13 +24,16 @@ describe('can solve for square matrices', function() {
       (pivot) => [pivot - 1, pivot + 1],
       R.filter(isInBounds(rank))
     );
-    var activateAdjacentToPivot = R.map((row) => {
-      var updateRowIndex = R.forEach((index) => row[index] = adjacentValue);
-      var updateAdjacentIndexes = R.pipe(findAdjacentIndexes, updateRowIndex);
-      updateAdjacentIndexes(row);
+    var assignAdjacentValuesToPivot = R.map((row) => {
+      var setAdjacentValueAtIndexes = R.forEach((index) => row[index] = adjacentValue);
+      var findAndSetAdjacentValuesForRow = R.pipe(
+        findAdjacentIndexes,
+        setAdjacentValueAtIndexes
+      );
+      findAndSetAdjacentValuesForRow(row);
       return row;
     });
-    return activateAdjacentToPivot(matrix);
+    return assignAdjacentValuesToPivot(matrix);
   };
 
   var makeA = (rank) => {
@@ -43,6 +46,46 @@ describe('can solve for square matrices', function() {
   };
 
   var mod2 = (x) => x % 2;
+
+  var modinv = (a, m) => {
+    var v = 1;
+    var d = a;
+    var u = (a === 1);
+    var t = 1 - u;
+    if (t === 1) {
+      var c = m % a;
+      u = Math.floor(m / a);
+      while (c !== 1 && t === 1) {
+        var q = Math.floor(d / c);
+        d = d % c;
+        v = v + q * u;
+        t = (d !== 1);
+        if (t === 1) {
+          q = Math.floor(c / d);
+          c = c % d;
+          u = u + q * v;
+        }
+      }
+      u = v * (1 - t) + t * (m - u);
+    }
+    return u;
+  };
+
+  var solveForX = (A, b) => {
+    //TODO: implement gaussian elimination mod 2
+
+    //multiply regular inverse by determinant, round to integers
+    //then multiply everything by the determinant's multiplicative inverse modulo 2
+    var det = A.det();
+    console.log(det);
+    var multiplicativeInverseMod2 = modinv(det, 2);
+    var invMod2 = A.inv()
+          .multiply(det)
+          .round()
+          .multiply(multiplicativeInverseMod2)
+          .map(R.compose(Math.abs, mod2));
+    return invMod2.multiply(b).map(mod2);
+  };
 
   //example when solving Ax=b for 3x3
 
@@ -75,7 +118,7 @@ describe('can solve for square matrices', function() {
   //    0 1 1
 
   //solve for x and we have our strategy to turn the lights off
-  it('3x3', function() {
+  it('specific 3x3', function() {
     var A = Matrix.create(makeA(3));
 
     var boardConfiguration = R.flatten(
@@ -90,7 +133,31 @@ describe('can solve for square matrices', function() {
     A.multiply(knownAnswer).map(mod2).elements.should.eql(b.elements);
 
     //and also use the board configuration to find the answer
-    var x = A.inv().round().multiply(b).map(mod2);
+    var x = solveForX(A, b);
     x.should.eql(knownAnswer);
   });
+
+  it("any random 2x2", function() {
+    var A = Matrix.create(makeA(2));
+    var b = Matrix.Random(4, 1).round();
+    var x = solveForX(A, b);
+    A.multiply(x).map(mod2).should.eql(b);
+  });
+
+  it("any random 3x3", function() {
+    var A = Matrix.create(makeA(3));
+    var b = Matrix.Random(9, 1).round();
+    var x = solveForX(A, b);
+
+    A.multiply(x).map(mod2).should.eql(b);
+  });
+
+  //rounding errors make this not work, need to implement gaussian elimination mod 2
+  xit("any random 4x4", function() {
+    var A = Matrix.create(makeA(4));
+    var b = Matrix.Random(16, 1).round();
+    var x = solveForX(A, b);
+    A.multiply(x).map(mod2).should.eql(b);
+  });
+
 });
